8edc638b17d2f20e8157c2768872ef8aa7484ba7
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2023-09-14 09:58:17 +0200

    Initial commit

---
+++ src/cat/insvidreres/inmrec/projecte_espai/UI/WindowManager.java
@@ -0,0 +1,4 @@
+package cat.insvidreres.inmrec.projecte_espai.UI;
+
+public class WindowManager {
+}
---
+++ src/cat/insvidreres/inmrec/projecte_espai/init/Start.java
@@ -0,0 +1,7 @@
+package cat.insvidreres.inmrec.projecte_espai.init;
+
+public class Start {
+    public static void main(String[] args) {
+
+    }
+}
