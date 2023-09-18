cfe06c1c59288ea75c0c272bf1627df6b92bd2f8
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2023-09-18 13:17:32 +0200

    interface added

---
+++ src/cat/insvidreres/inmrec/projecte_espai/classes/Agent.java
@@ -0,0 +1,8 @@
+package cat.insvidreres.inmrec.projecte_espai.classes;
+
+public interface Agent {
+
+    default void enviarMissatge(String codi_professional) {
+
+    }
+}
--- src/cat/insvidreres/inmrec/projecte_espai/classes/Mecanic.java
+++ src/cat/insvidreres/inmrec/projecte_espai/classes/Mecanic.java
@@ -1,6 +1,11 @@
 package cat.insvidreres.inmrec.projecte_espai.classes;
 
 public class Mecanic extends Persona {
+
+    private String adreca, ciutat;
+    private int salari, edat, numero;
+    private char sexe;
+
     public Mecanic(String nom, String codi_professional) {
         super(nom, codi_professional);
     }
