8ca42480de2975ff43216bd25c178525dd5a6ad3
ELOWIS37 <eloy.castano@institutvidreres.cat>
2023-09-18 14:17:31 +0200

    Login GUI

--- src/cat/insvidreres/inmrec/projecte_espai/UI/WindowManager.java
+++ src/cat/insvidreres/inmrec/projecte_espai/UI/WindowManager.java
@@ -1,4 +1,34 @@
 package cat.insvidreres.inmrec.projecte_espai.UI;
 
-public class WindowManager {
+import javax.swing.*;
+
+public class WindowManager extends JFrame{
+    
+    public WindowManager() {
+        initComponents();
+    }
+    
+     private void initComponents() {
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("PES");
+        
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        tabbedPane.addTab("DADES", new DadesPanel());
+        tabbedPane.addTab("CRUD", new CrudPanel());
+        
+    }
+}
+class DadesPanel extends JPanel {
+    public DadesPanel(){
+        JButton b1 = new JButton("PROVA1");
+        add(b1);
+    }
+}
+
+class CrudPanel extends JPanel {
+    public CrudPanel(){
+        JButton b1 = new JButton("PROVA2");
+        add(b1);
+    }
 }
