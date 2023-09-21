b5ee23854f8d4237e65897fe9fed122b1eab3083
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2023-09-21 11:07:03 +0200

    temp

--- src/cat/insvidreres/inmrec/projecte_espai/engine/Queries.java
+++ src/cat/insvidreres/inmrec/projecte_espai/engine/Queries.java
@@ -1,19 +1,41 @@
 package cat.insvidreres.inmrec.projecte_espai.engine;
 
+import cat.insvidreres.inmrec.projecte_espai.classes.Agent;
 import cat.insvidreres.inmrec.projecte_espai.init.Start;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
-public class Queries {
+public class Queries implements Agent {
 
     private static String SQLSentence;
     private static Connection connection = Start.getConnection();
-    private static Statement statement;
+    private static PreparedStatement statement;
     private static ResultSet result;
 
     public static void insertCodedMessage(String codi_professional, String message, String categoria) {
+
+        SQLSentence = "INSERT INTO missatge (emissor, contingut_missatge, categoria_emissor) VALUES (?, ?, ?)";
+
+        try {
+            statement = connection.prepareStatement(SQLSentence);
+            statement.setString(1, codi_professional);
+            statement.setString(2, message);
+            statement.setString(3, categoria);
+
+            int rowsInserted = statement.executeUpdate(SQLSentence);
+
+            if (rowsInserted > 1)
+                System.out.println("Rows inserted correctly");
+            else
+                System.out.println("Error in inserting");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if ()
+            }
+        }
 
     }
 
